f2f0714c411c334012a0e2d5b5d66926bcb3bd41
ANkoR911 <vladimir-bredikhin0@rambler.ru>
2015-05-01 13:14:12 +0300

    Точка, отрезок, вектор

---
+++ Точка, отрезок, вектор.cpp
@@ -0,0 +1,287 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+class Point
+{
+private:
+	double x;
+	double y;
+
+public:
+	Point() : x(0), y(0)
+	{}
+
+	Point(double a, double b) : x(a), y(b)
+	{}
+
+	Point operator = (const Point &a)
+	{
+		x = a.x;
+		y = a.y;
+		return *this;
+	}
+
+	double ShowX()
+	{
+		return x;
+	}
+
+	double ShowY()
+	{
+		return y;
+	}
+
+	void PrintPoint()
+	{
+		cout << "(" << x << ", " << y << ")" << endl;
+	}
+};
+
+class Vector
+{
+private:
+	double VecCoordX;
+	double VecCoordY;
+
+public:
+	Vector() : VecCoordX(0), VecCoordY(0)
+	{}
+
+	Vector(double a, double b)
+	{
+		VecCoordX = a;
+		VecCoordY = b;
+	}
+
+	Vector(Point a1, Point b1)
+	{
+		VecCoordX = b1.ShowX() - a1.ShowX();
+		VecCoordY = b1.ShowY() - a1.ShowY();
+	}
+
+	void Reverse()
+	{
+		VecCoordX *= -1;
+		VecCoordY *= -1;
+	}
+
+	double Mod()
+	{
+		return sqrt(VecCoordX * VecCoordX + VecCoordY * VecCoordY);
+	}
+
+	void MakeToOneSize()
+	{
+		double Size = sqrt(VecCoordX * VecCoordX + VecCoordY * VecCoordY);
+		VecCoordX /= Size;
+		VecCoordY /= Size;
+	}
+
+	void PrintVector()
+	{
+		cout << VecCoordX << " " << VecCoordY << endl;
+	}
+
+	Vector operator = (const Vector &a)
+	{
+		VecCoordX = a.VecCoordX;
+		VecCoordY = a.VecCoordY;
+		return *this;
+	}
+
+	Vector operator *= (double &a)
+	{
+		VecCoordX *= a;
+		VecCoordY *= a;
+		return *this;
+	}
+
+	bool operator == (const Vector &a)
+	{
+		if ((VecCoordX == a.VecCoordX) && (VecCoordY == a.VecCoordY)) return true;
+		else return false;
+	}
+
+	bool operator != (const Vector &a)
+	{
+		if (*this == a) return false;
+		else return true;
+	}
+
+	bool VecOneWay(const Vector foo)
+	{
+		Vector a = *this;
+		Vector b = foo;
+		double Help1, Help2;
+		Help1 = fabs(a.VecCoordX);
+		Help2 = fabs(b.VecCoordX);
+		a *= Help2;
+		b *= Help1;
+		if (a != b)
+		{
+				return false;
+		}
+		else return true;
+	}
+
+	bool Parallel(const Vector foo)
+	{
+		Vector a = *this;
+		Vector b = foo;
+		double Help1, Help2;
+		Help1 = fabs(a.VecCoordX);
+		Help2 = fabs(b.VecCoordX);
+		a *= Help2;
+		b *= Help1;
+		if (a != b)
+		{
+			b.Reverse();
+			if (a != b)
+				return false;
+			else return true;
+		}
+		else return true;
+	}
+};
+
+class Segment
+{
+private:
+	Point A;
+	Point B;
+
+public:
+	Segment(Point a, Point b) : A(a), B(b)
+	{};
+
+	Segment operator = (const Segment &a)
+	{
+		A = a.A;
+		B = a.B;
+		return *this;
+	}
+
+	Vector FindVector()
+	{
+		Vector Rez(B.ShowX() - A.ShowX(), B.ShowY() - A.ShowY());
+		return Rez;
+	}
+
+	bool PointAccessory(Point a)
+	{
+		double x1, x2, x3, y1, y2, y3, S;
+		Vector TestOne(a, A);
+		
+		Vector TestTwo(a, B);
+
+		x1 = a.ShowX();
+		y1 = a.ShowY();
+		x2 = A.ShowX();
+		y2 = A.ShowY();
+		x3 = B.ShowX();
+		y3 = B.ShowY();
+		S = (x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 -y2)) / 2;
+		if ((S == 0) && ((TestOne.VecOneWay(TestTwo)) != 1)) return true;
+		else return false;
+	}
+
+	void PrintSegment()
+	{
+		cout << "First Point - " << "(" << A.ShowX() << ", " << A.ShowY() << ") ";
+		cout << "Second Point - " << "(" << B.ShowX() << ", " << B.ShowY() << ")" << endl;
+	}
+
+	void FindIntersectionWith(Segment Two)
+	{
+		double A1, B1, C1, A2, B2, C2, X1, Y1;
+		Segment One = *this;
+		Vector FromOne, FromTwo;
+		FromOne = One.FindVector();
+		FromTwo = Two.FindVector();
+		if (FromOne.Parallel(FromTwo))
+		{
+			Point OneA = One.A;
+			Point OneB = One.B;
+			Point TwoA = Two.A;
+			Point TwoB = Two.B;			
+			if ((Two.PointAccessory(OneA)) && (Two.PointAccessory(OneB)))
+			{					
+				cout << "The intersection is Segment - ";
+				One.PrintSegment();
+			}
+			else
+			{
+				if ((One.PointAccessory(TwoA)) && (One.PointAccessory(TwoB)))
+				{
+					cout << "The intersection is Segment - ";
+					Two.PrintSegment();
+				}
+				else
+				{
+					if ((One.PointAccessory(TwoA)) && (Two.PointAccessory(OneB)))
+					{
+						cout << "The intersection is Segment - ";
+						Segment Rez(TwoA, OneB);
+						Rez.PrintSegment();
+					}
+					else
+					{
+						if ((Two.PointAccessory(OneA)) && (One.PointAccessory(TwoB)))
+						{
+							cout << "The intersection is Segment - ";
+							Segment Rez(OneA, TwoB);
+							Rez.PrintSegment();
+						}
+						else
+						{
+							cout << "The intersection is Empty set." << endl;
+						}
+					}
+				}
+			}		
+			
+		}
+		else
+		{
+			cout << "The intersection is Point - ";
+			A1 = One.A.ShowY() - One.B.ShowY();
+			B1 = One.B.ShowX() - One.A.ShowX();
+			C1 = One.A.ShowX()*One.B.ShowY() - One.B.ShowX()*One.A.ShowY();
+			A2 = Two.A.ShowY() - Two.B.ShowY();
+			B2 = Two.B.ShowX() - Two.A.ShowX();
+			C2 = Two.A.ShowX()*Two.B.ShowY() - Two.B.ShowX()*Two.A.ShowY();
+			X1 = -((C1*B2 - C2*B1) / (A1*B2 - A2*B1));
+			Y1 = -((A1*C2 - C1*A2) / (A1*B2 - B1*A2));
+			Point Rez(X1, Y1);
+			Rez.PrintPoint();
+		}
+	}
+};
+
+
+
+
+int main()
+{
+	double a, b, c, d, e, f, g, h;
+	cin >> a >> b >> c >> d >> e >> f >> g >> h;
+	Point A(a, b);
+	Point B(c, d);
+	Point C(e, f);
+	Point D(g, h);
+	Segment O1(A, B);
+	Segment O2(C, D);
+	//bool k = O.PointAccessory(C);
+	//Vector Test;
+	//Test = O.FindVector();
+	A.PrintPoint();
+	B.PrintPoint();
+	O1.PrintSegment();
+	O2.PrintSegment();
+	O1.FindIntersectionWith(O2);
+	//cout << k << endl;
+	//Test.PrintVector();
+	return 0;
+}
